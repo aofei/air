087d561ea8671676ea565d5d3929a42d18b81f61
Aofei Sheng <aofei@sheng.ws>
2016-08-02 17:21:29 +0800

    style: beautify air.go and group.go

--- group.go
+++ group.go
@@ -2,7 +2,7 @@ package air
 
 // Group is a set of sub-routes for a specified route. It can be used for inner
 // routes that share a common gas or functionality that should be separate
-// from the parent air instance while still inheriting from it.
+// from the parent `Air` instance while still inheriting from it.
 type Group struct {
 	prefix string
 	gases  []GasFunc
@@ -16,7 +16,15 @@ func NewGroup(prefix string, a *Air, gases ...GasFunc) *Group {
 	return g
 }
 
-// Contain implements `Air#Contain()` for sub-routes within the Group.
+// NewSubGroup creates a new sub-group with prefix and optional sub-group-level gases.
+func (g *Group) NewSubGroup(prefix string, gases ...GasFunc) *Group {
+	gs := []GasFunc{}
+	gs = append(gs, g.gases...)
+	gs = append(gs, gases...)
+	return NewGroup(g.prefix+prefix, g.air, gs...)
+}
+
+// Contain implements `Air#Contain()`.
 func (g *Group) Contain(gases ...GasFunc) {
 	g.gases = append(g.gases, gases...)
 	// Allow all requests to reach the group as they might get dropped if router
@@ -28,44 +36,37 @@ func (g *Group) Contain(gases ...GasFunc) {
 	}
 }
 
-// Get implements `Air#Get()` for sub-routes within the Group.
+// Get implements `Air#Get()`.
 func (g *Group) Get(path string, handler HandlerFunc, gases ...GasFunc) {
 	g.add(GET, path, handler, gases...)
 }
 
-// Post implements `Air#Post()` for sub-routes within the Group.
+// Post implements `Air#Post()`.
 func (g *Group) Post(path string, handler HandlerFunc, gases ...GasFunc) {
 	g.add(POST, path, handler, gases...)
 }
 
-// Put implements `Air#Put()` for sub-routes within the Group.
+// Put implements `Air#Put()`.
 func (g *Group) Put(path string, handler HandlerFunc, gases ...GasFunc) {
 	g.add(PUT, path, handler, gases...)
 }
 
-// Delete implements `Air#Delete()` for sub-routes within the Group.
+// Delete implements `Air#Delete()`.
 func (g *Group) Delete(path string, handler HandlerFunc, gases ...GasFunc) {
 	g.add(DELETE, path, handler, gases...)
 }
 
-// NewSubGroup creates a new sub-group with prefix and optional sub-group-level gas.
-func (g *Group) NewSubGroup(prefix string, gases ...GasFunc) *Group {
-	gs := []GasFunc{}
-	gs = append(gs, g.gases...)
-	gs = append(gs, gases...)
-	return NewGroup(g.prefix+prefix, g.air, gs...)
-}
-
-// Static implements `Air#Static()` for sub-routes within the Group.
+// Static implements `Air#Static()`.
 func (g *Group) Static(prefix, root string) {
 	g.air.Static(g.prefix+prefix, root)
 }
 
-// File implements `Air#File()` for sub-routes within the Group.
+// File implements `Air#File()`.
 func (g *Group) File(path, file string) {
 	g.air.File(g.prefix+path, file)
 }
 
+// add implements `Air#add()`.
 func (g *Group) add(method, path string, handler HandlerFunc, gases ...GasFunc) {
 	// Combine into a new slice, to avoid accidentally passing the same
 	// slice for multiple routes, which would lead to later add() calls overwriting
