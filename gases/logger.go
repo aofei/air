292e5cb66ce6f458809a1c472cd133cef407a0f9
Aofei Sheng <aofei@aofeisheng.com>
2016-08-18 12:14:08 +0800

    refactor: use built-in package "text/template" for logger
    This removes the third-party package "github.com/valyala/fasttemplate".

--- gases/logger.go
+++ gases/logger.go
@@ -5,13 +5,11 @@ import (
 	"io"
 	"net"
 	"os"
-	"strconv"
 	"sync"
+	"text/template"
 	"time"
 
 	"github.com/sheng/air"
-
-	"github.com/valyala/fasttemplate"
 )
 
 type (
@@ -37,7 +35,7 @@ type (
 		// - bytes_in (Bytes received)
 		// - bytes_out (Bytes sent)
 		//
-		// Example "${remote_ip} ${status}"
+		// Example "{{.remote_ip}} {{.status}}"
 		//
 		// Optional. Default value DefaultLoggerConfig.Format.
 		Format string `json:"format"`
@@ -46,7 +44,7 @@ type (
 		// Optional. Default value os.Stdout.
 		Output io.Writer
 
-		template   *fasttemplate.Template
+		template   *template.Template
 		bufferPool sync.Pool
 	}
 )
@@ -55,10 +53,10 @@ var (
 	// DefaultLoggerConfig is the default Logger gas config.
 	DefaultLoggerConfig = LoggerConfig{
 		Skipper: defaultSkipper,
-		Format: `{"time":"${time_rfc3339}","remote_ip":"${remote_ip}",` +
-			`"method":"${method}","uri":"${uri}","status":${status}, "latency":${latency},` +
-			`"latency_human":"${latency_human}","bytes_in":${bytes_in},` +
-			`"bytes_out":${bytes_out}}` + "\n",
+		Format: `{"time":"{{.time_rfc3339}}","remote_ip":"{{.remote_ip}}",` +
+			`"method":"{{.method}}","uri":"{{.uri}}","status":{{.status}},` +
+			`"latency":{{.latency}},"latency_human":"{{.latency_human}}",` +
+			`"bytes_in":{{.bytes_in}},"bytes_out":{{.bytes_out}}}` + "\n",
 		Output: os.Stdout,
 	}
 )
@@ -82,7 +80,7 @@ func LoggerWithConfig(config LoggerConfig) air.GasFunc {
 		config.Output = DefaultLoggerConfig.Output
 	}
 
-	config.template = fasttemplate.New(config.Format, "${", "}")
+	config.template, _ = template.New("logger").Parse(config.Format)
 	config.bufferPool = sync.Pool{
 		New: func() interface{} {
 			return bytes.NewBuffer(make([]byte, 256))
@@ -106,55 +104,37 @@ func LoggerWithConfig(config LoggerConfig) air.GasFunc {
 			buf.Reset()
 			defer config.bufferPool.Put(buf)
 
-			_, err = config.template.ExecuteFunc(buf, func(w io.Writer, tag string) (int, error) {
-				switch tag {
-				case "time_rfc3339":
-					return w.Write([]byte(time.Now().Format(time.RFC3339)))
-				case "remote_ip":
-					ra := req.RemoteAddress()
-					if ip := req.Header.Get(air.HeaderXRealIP); ip != "" {
-						ra = ip
-					} else if ip = req.Header.Get(air.HeaderXForwardedFor); ip != "" {
-						ra = ip
-					} else {
-						ra, _, _ = net.SplitHostPort(ra)
-					}
-					return w.Write([]byte(ra))
-				case "host":
-					return w.Write([]byte(req.Host()))
-				case "uri":
-					return w.Write([]byte(req.RequestURI()))
-				case "method":
-					return w.Write([]byte(req.Method()))
-				case "path":
-					p := req.URI.Path()
-					if p == "" {
-						p = "/"
-					}
-					return w.Write([]byte(p))
-				case "referer":
-					return w.Write([]byte(req.Referer()))
-				case "user_agent":
-					return w.Write([]byte(req.UserAgent()))
-				case "status":
-					n := c.StatusCode
-					return w.Write([]byte(strconv.Itoa(n)))
-				case "latency":
-					l := stop.Sub(start).Nanoseconds() / 1000
-					return w.Write([]byte(strconv.FormatInt(l, 10)))
-				case "latency_human":
-					return w.Write([]byte(stop.Sub(start).String()))
-				case "bytes_in":
-					b := req.Header.Get(air.HeaderContentLength)
-					if b == "" {
-						b = "0"
-					}
-					return w.Write([]byte(b))
-				case "bytes_out":
-					return w.Write([]byte(strconv.FormatInt(res.Size, 10)))
-				}
-				return 0, nil
-			})
+			data := make(map[string]interface{})
+			data["time_rfc3339"] = time.Now().Format(time.RFC3339)
+			ra := req.RemoteAddress()
+			if ip := req.Header.Get(air.HeaderXRealIP); ip != "" {
+				ra = ip
+			} else if ip = req.Header.Get(air.HeaderXForwardedFor); ip != "" {
+				ra = ip
+			} else {
+				ra, _, _ = net.SplitHostPort(ra)
+			}
+			data["remote_ip"] = ra
+			data["host"] = req.Host()
+			data["uri"] = req.RequestURI()
+			data["method"] = req.Method()
+			p := req.URI.Path()
+			if p == "" {
+				p = "/"
+			}
+			data["path"] = p
+			data["referer"] = req.Referer()
+			data["user_agent"] = req.UserAgent()
+			data["status"] = c.StatusCode
+			data["latency"] = stop.Sub(start).Nanoseconds() / 1000
+			data["latency_human"] = stop.Sub(start).String()
+			b := req.Header.Get(air.HeaderContentLength)
+			if b == "" {
+				b = "0"
+			}
+			data["bytes_in"] = b
+			data["bytes_out"] = res.Size
+			err = config.template.Execute(buf, data)
 			if err == nil {
 				config.Output.Write(buf.Bytes())
 			}
