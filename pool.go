8ef170a01eec24038a2e6445d87887235721fbdb
Aofei Sheng <aofei@aofeisheng.com>
2016-11-17 16:05:42 +0800

    refactor: remove fasthttp

--- pool.go
+++ pool.go
@@ -4,13 +4,7 @@ import "sync"
 
 // Pool represents the pools of `Air`.
 type Pool struct {
-	contextPool        *sync.Pool
-	requestPool        *sync.Pool
-	responsePool       *sync.Pool
-	requestHeaderPool  *sync.Pool
-	responseHeaderPool *sync.Pool
-	uriPool            *sync.Pool
-	cookiePool         *sync.Pool
+	contextPool *sync.Pool
 }
 
 // newPool returnes a new instance of `Pool`.
@@ -21,36 +15,6 @@ func newPool(a *Air) *Pool {
 				return newContext(a)
 			},
 		},
-		requestPool: &sync.Pool{
-			New: func() interface{} {
-				return newRequest(a)
-			},
-		},
-		responsePool: &sync.Pool{
-			New: func() interface{} {
-				return newResponse(a)
-			},
-		},
-		requestHeaderPool: &sync.Pool{
-			New: func() interface{} {
-				return newRequestHeader()
-			},
-		},
-		responseHeaderPool: &sync.Pool{
-			New: func() interface{} {
-				return newResponseHeader()
-			},
-		},
-		uriPool: &sync.Pool{
-			New: func() interface{} {
-				return newURI()
-			},
-		},
-		cookiePool: &sync.Pool{
-			New: func() interface{} {
-				return newCookie()
-			},
-		},
 	}
 }
 
@@ -59,59 +23,11 @@ func (p *Pool) Context() *Context {
 	return p.contextPool.Get().(*Context)
 }
 
-// Request returns an empty instance of `Request` from p.
-func (p *Pool) Request() *Request {
-	return p.requestPool.Get().(*Request)
-}
-
-// Response returns an empty instance of `Response` from p.
-func (p *Pool) Response() *Response {
-	return p.responsePool.Get().(*Response)
-}
-
-// RequestHeader returns an empty instance of `RequestHeader` from p.
-func (p *Pool) RequestHeader() *RequestHeader {
-	return p.requestHeaderPool.Get().(*RequestHeader)
-}
-
-// ResponseHeader returns an empty instance of `ResponseHeader` from p.
-func (p *Pool) ResponseHeader() *ResponseHeader {
-	return p.responseHeaderPool.Get().(*ResponseHeader)
-}
-
-// URI returns an empty instance of `URI` from p.
-func (p *Pool) URI() *URI {
-	return p.uriPool.Get().(*URI)
-}
-
-// Cookie returns an empty instance of `Cookie` from p.
-func (p *Pool) Cookie() *Cookie {
-	return p.cookiePool.Get().(*Cookie)
-}
-
 // Put puts x back to p.
 func (p *Pool) Put(x interface{}) {
 	switch v := x.(type) {
 	case *Context:
 		v.reset()
 		p.contextPool.Put(v)
-	case *Request:
-		v.reset()
-		p.requestPool.Put(v)
-	case *Response:
-		v.reset()
-		p.responsePool.Put(v)
-	case *RequestHeader:
-		v.reset()
-		p.requestHeaderPool.Put(v)
-	case *ResponseHeader:
-		v.reset()
-		p.responseHeaderPool.Put(v)
-	case *URI:
-		v.reset()
-		p.uriPool.Put(v)
-	case *Cookie:
-		v.reset()
-		p.cookiePool.Put(v)
 	}
 }
--- server.go
+++ server.go
@@ -1,70 +1,75 @@
 package air
 
-import "github.com/valyala/fasthttp"
+import (
+	"crypto/tls"
+	"net"
+	"net/http"
+	"time"
+)
 
 // server represents the HTTP server.
 type server struct {
-	fastServer *fasthttp.Server
-	air        *Air
+	*http.Server
+	air *Air
 }
 
 // newServer returns a new instance of `server`.
 func newServer(a *Air) *server {
 	s := &server{
-		fastServer: &fasthttp.Server{},
-		air:        a,
+		Server: &http.Server{},
+		air:    a,
 	}
-	s.fastServer.ReadTimeout = s.air.Config.ReadTimeout
-	s.fastServer.WriteTimeout = s.air.Config.WriteTimeout
-	s.fastServer.Handler = s.serveHTTP
+	s.Addr = s.air.Config.Address
+	s.Handler = s
+	s.ReadTimeout = s.air.Config.ReadTimeout
+	s.WriteTimeout = s.air.Config.WriteTimeout
 	return s
 }
 
 // start starts the HTTP server.
 func (s *server) start() error {
 	if s.air.Config.Listener == nil {
-		return s.startDefaultListener()
-	}
-	return s.startCustomListener()
+		ln, err := net.Listen("tcp", s.air.Config.Address)
+		if err != nil {
+			return err
+		}
 
+		if s.air.Config.TLSCertFile != "" &&
+			s.air.Config.TLSKeyFile != "" {
+			config := &tls.Config{}
+			if !s.air.Config.DisableHTTP2 {
+				config.NextProtos = append(config.NextProtos, "h2")
+			}
+			config.Certificates = make([]tls.Certificate, 1)
+			config.Certificates[0], err = tls.LoadX509KeyPair(s.air.Config.TLSCertFile,
+				s.air.Config.TLSKeyFile)
+			if err != nil {
+				return err
+			}
+			s.air.Config.Listener = tls.NewListener(tcpKeepAliveListener{ln.(*net.TCPListener)}, config)
+		} else {
+			s.air.Config.Listener = tcpKeepAliveListener{ln.(*net.TCPListener)}
+		}
+	}
+	return s.Serve(s.air.Config.Listener)
 }
 
-// startDefaultListener starts the default HTTP linsterner.
-func (s *server) startDefaultListener() error {
-	c := s.air.Config
-	if c.TLSCertFile != "" && c.TLSKeyFile != "" {
-		return s.fastServer.ListenAndServeTLS(c.Address, c.TLSCertFile, c.TLSKeyFile)
-	}
-	return s.fastServer.ListenAndServe(c.Address)
+func (s *server) stop() error {
+	return s.air.Config.Listener.Close()
 }
 
-// startCustomListener starts the custom HTTP linsterner.
-func (s *server) startCustomListener() error {
-	c := s.air.Config
-	if c.TLSCertFile != "" && c.TLSKeyFile != "" {
-		return s.fastServer.ServeTLS(c.Listener, c.TLSCertFile, c.TLSKeyFile)
-	}
-	return s.fastServer.Serve(c.Listener)
-}
-
-// serveHTTP serves the HTTP requests.
-func (s *server) serveHTTP(fastCtx *fasthttp.RequestCtx) {
+// ServeHTTP implements `http.Handler`.
+func (s *server) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 	c := s.air.Pool.Context()
 
-	// Request
-	c.Request.fastCtx = fastCtx
-	c.Request.Header.fastRequestHeader = &fastCtx.Request.Header
-	c.Request.URI.fastURI = fastCtx.URI()
-
-	// Response
-	c.Response.fastCtx = fastCtx
-	c.Response.Header.fastResponseHeader = &fastCtx.Response.Header
-	c.Response.Writer = fastCtx
+	c.responseWriter = rw
+	c.Header = rw.Header()
+	c.Request = req
 
 	// Gases
 	h := func(c *Context) error {
-		method := c.Request.Method()
-		path := c.Request.URI.PathOriginal()
+		method := c.Request.Method
+		path := c.Request.URL.EscapedPath()
 		s.air.router.route(method, path, c)
 		h := c.Handler
 		for i := len(s.air.gases) - 1; i >= 0; i-- {
@@ -84,4 +89,22 @@ func (s *server) serveHTTP(fastCtx *fasthttp.RequestCtx) {
 	}
 
 	s.air.Pool.Put(c)
+}
+
+// tcpKeepAliveListener sets TCP keep-alive timeouts on accepted
+// connections. It's used by ListenAndServe and ListenAndServeTLS so
+// dead TCP connections (e.g. closing laptop mid-download) eventually
+// go away.
+type tcpKeepAliveListener struct {
+	*net.TCPListener
+}
+
+func (ln tcpKeepAliveListener) Accept() (c net.Conn, err error) {
+	tc, err := ln.AcceptTCP()
+	if err != nil {
+		return
+	}
+	tc.SetKeepAlive(true)
+	tc.SetKeepAlivePeriod(3 * time.Minute)
+	return tc, nil
 }
