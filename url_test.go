98bdf8471a23ef0c718b7f6b49d8fa003095ddf4
Aofei Sheng <aofei@sheng.ws>
2017-01-18 21:05:42 +0800

    test: add url_test.go

---
+++ url_test.go
@@ -0,0 +1,25 @@
+package air
+
+import (
+	"net/http"
+	"net/url"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestURLQueryValue(t *testing.T) {
+	a := New()
+	c := newContext(a)
+
+	vs := make(url.Values)
+	vs.Set("name", "Air")
+	vs.Set("author", "Aofei Sheng")
+	req, _ := http.NewRequest(GET, "/?"+vs.Encode(), nil)
+
+	c.feed(req, nil)
+
+	assert.Equal(t, "Air", c.Request.URL.QueryValue("name"))
+	assert.Equal(t, "Aofei Sheng", c.Request.URL.QueryValue("author"))
+	assert.Equal(t, vs, c.Request.URL.QueryValues())
+}
