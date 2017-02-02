8a5701774fe438a682cf522240b3e8b6680b4b7a
Aofei Sheng <aofei@aofeisheng.com>
2017-02-02 10:55:25 +0800

    test: update config_test.go

--- config_test.go
+++ config_test.go
@@ -1,15 +1,14 @@
 package air
 
 import (
+	"os"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestConfigParseYAML(t *testing.T) {
-	c := newConfig()
-
+func TestConfigNewConfig(t *testing.T) {
 	yaml := `app_name: "air"` + "\n" +
 		`debug_mode: true` + "\n" +
 		`log_enabled: true` + "\n" +
@@ -26,7 +25,19 @@ func TestConfigParseYAML(t *testing.T) {
 		`template_right_delim: ">>"` + "\n" +
 		`minify_template: true`
 
-	c.Parse(yaml)
+	f, err := os.Create("config.yml")
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		f.Close()
+		os.Remove(f.Name())
+	}()
+
+	f.WriteString(yaml)
+
+	c := newConfig()
+
 	assert.Equal(t, "air", c.AppName)
 	assert.Equal(t, true, c.DebugMode)
 	assert.Equal(t, true, c.LogEnabled)
@@ -45,42 +56,30 @@ func TestConfigParseYAML(t *testing.T) {
 	assert.NotNil(t, c.Data)
 }
 
-func TestConfigParseJSON(t *testing.T) {
-	c := newConfig()
+func TestConfigParseError(t *testing.T) {
+	c := &Config{}
+	assert.Panics(t, func() {
+		c.Parse("\t")
+	})
+}
 
-	json := `{
-			"app_name": "air",
-			"debug_mode": true,
-			"log_enabled": true,
-			"log_format": "air_log",
-			"address": "127.0.0.1:2333",
-			"disable_http2": true,
-			"tls_cert_file": "path_to_tls_cert_file",
-			"tls_key_file": "path_to_tls_key_file",
-			"read_timeout": 60,
-			"write_timeout": 60,
-			"template_root": "ts",
-			"template_ext": ".tmpl",
-			"template_left_delim": "<<",
-			"template_right_delim": ">>",
-			"minify_template": true
-		 }`
+func TestConfigParseFileError(t *testing.T) {
+	c := &Config{}
 
-	c.Parse(json)
-	assert.Equal(t, "air", c.AppName)
-	assert.Equal(t, true, c.DebugMode)
-	assert.Equal(t, true, c.LogEnabled)
-	assert.Equal(t, "air_log", c.LogFormat)
-	assert.Equal(t, "127.0.0.1:2333", c.Address)
-	assert.Equal(t, true, c.DisableHTTP2)
-	assert.Equal(t, "path_to_tls_cert_file", c.TLSCertFile)
-	assert.Equal(t, "path_to_tls_key_file", c.TLSKeyFile)
-	assert.Equal(t, 60*time.Second, c.ReadTimeout)
-	assert.Equal(t, 60*time.Second, c.WriteTimeout)
-	assert.Equal(t, "ts", c.TemplateRoot)
-	assert.Equal(t, ".tmpl", c.TemplateExt)
-	assert.Equal(t, "<<", c.TemplateLeftDelim)
-	assert.Equal(t, ">>", c.TemplateRightDelim)
-	assert.Equal(t, true, c.MinifyTemplate)
-	assert.NotNil(t, c.Data)
+	assert.Panics(t, func() {
+		c.ParseFile("config_not_exist.yml")
+	})
+
+	dn := "config_is_a_directory.yml"
+	err := os.Mkdir(dn, os.ModeDir)
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		os.Remove(dn)
+	}()
+
+	assert.Panics(t, func() {
+		c.ParseFile(dn)
+	})
 }
