e06f26f0e43409ac2cab2e86e284c486802c3ee7
Aofei Sheng <aofei@sheng.ws>
2017-02-21 18:34:05 +0800

    test: update response_test.go

--- response_test.go
+++ response_test.go
@@ -12,6 +12,24 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestResponseSetCookie(t *testing.T) {
+	a := New()
+	req, _ := http.NewRequest(GET, "/", nil)
+	rec := httptest.NewRecorder()
+	c := NewContext(a)
+
+	c.feed(req, rec)
+
+	cookie := &http.Cookie{
+		Name:  "Air",
+		Value: "An ideal RESTful web framework for Go.",
+	}
+
+	c.SetCookie(cookie)
+
+	assert.Equal(t, cookie.String(), c.Response.Header().Get(HeaderSetCookie))
+}
+
 func TestResponseRender(t *testing.T) {
 	a := New()
 	req, _ := http.NewRequest(GET, "/", nil)
@@ -24,9 +42,9 @@ func TestResponseRender(t *testing.T) {
 	r.template = template.Must(template.New("info").Parse("{{.name}} by {{.author}}."))
 	a.Renderer = r
 
-	c.Response.Data["name"] = "Air"
-	c.Response.Data["author"] = "Aofei Sheng"
-	if err := c.Response.Render("info"); assert.NoError(t, err) {
+	c.Data["name"] = "Air"
+	c.Data["author"] = "Aofei Sheng"
+	if err := c.Render("info"); assert.NoError(t, err) {
 		assert.Equal(t, http.StatusOK, rec.Code)
 		assert.Equal(t, MIMETextHTML, rec.Header().Get(HeaderContentType))
 		assert.Equal(t, "Air by Aofei Sheng.", rec.Body.String())
@@ -41,7 +59,7 @@ func TestResponseHTML(t *testing.T) {
 
 	c.feed(req, rec)
 
-	if err := c.Response.HTML("Air"); assert.NoError(t, err) {
+	if err := c.HTML("Air"); assert.NoError(t, err) {
 		assert.Equal(t, http.StatusOK, rec.Code)
 		assert.Equal(t, MIMETextHTML, rec.Header().Get(HeaderContentType))
 		assert.Equal(t, "Air", rec.Body.String())
@@ -56,7 +74,7 @@ func TestResponseString(t *testing.T) {
 
 	c.feed(req, rec)
 
-	if err := c.Response.String("Air"); assert.NoError(t, err) {
+	if err := c.String("Air"); assert.NoError(t, err) {
 		assert.Equal(t, http.StatusOK, rec.Code)
 		assert.Equal(t, MIMETextPlain, rec.Header().Get(HeaderContentType))
 		assert.Equal(t, "Air", rec.Body.String())
@@ -65,6 +83,7 @@ func TestResponseString(t *testing.T) {
 
 func TestResponseJSON(t *testing.T) {
 	a := New()
+	a.Config.DebugMode = true
 	req, _ := http.NewRequest(GET, "/", nil)
 	rec := httptest.NewRecorder()
 	c := NewContext(a)
@@ -72,8 +91,12 @@ func TestResponseJSON(t *testing.T) {
 	c.feed(req, rec)
 
 	info := struct{ Name, Author string }{"Air", "Aofei Sheng"}
-	infoStr := `{"Name":"Air","Author":"Aofei Sheng"}`
-	if err := c.Response.JSON(info); assert.NoError(t, err) {
+	infoStr := `{
+	"Name": "Air",
+	"Author": "Aofei Sheng"
+}`
+
+	if err := c.JSON(info); assert.NoError(t, err) {
 		assert.Equal(t, http.StatusOK, rec.Code)
 		assert.Equal(t, MIMEApplicationJSON, rec.Header().Get(HeaderContentType))
 		assert.Equal(t, infoStr, rec.Body.String())
@@ -91,7 +114,7 @@ func TestResponseJSONP(t *testing.T) {
 	info := struct{ Name, Author string }{"Air", "Aofei Sheng"}
 	infoStr := `{"Name":"Air","Author":"Aofei Sheng"}`
 	cb := "callback"
-	if err := c.Response.JSONP(info, cb); assert.NoError(t, err) {
+	if err := c.JSONP(info, cb); assert.NoError(t, err) {
 		assert.Equal(t, http.StatusOK, rec.Code)
 		assert.Equal(t, MIMEApplicationJavaScript, rec.Header().Get(HeaderContentType))
 		assert.Equal(t, cb+"("+infoStr+");", rec.Body.String())
@@ -100,6 +123,7 @@ func TestResponseJSONP(t *testing.T) {
 
 func TestResponseXML(t *testing.T) {
 	a := New()
+	a.Config.DebugMode = true
 	req, _ := http.NewRequest(GET, "/", nil)
 	rec := httptest.NewRecorder()
 	c := NewContext(a)
@@ -108,11 +132,15 @@ func TestResponseXML(t *testing.T) {
 
 	type Info struct{ Name, Author string }
 	info := Info{"Air", "Aofei Sheng"}
-	infoStr := "<Info><Name>Air</Name><Author>Aofei Sheng</Author></Info>"
-	if err := c.Response.XML(info); assert.NoError(t, err) {
+	infoStr := xml.Header + `<Info>
+	<Name>Air</Name>
+	<Author>Aofei Sheng</Author>
+</Info>`
+
+	if err := c.XML(info); assert.NoError(t, err) {
 		assert.Equal(t, http.StatusOK, rec.Code)
 		assert.Equal(t, MIMEApplicationXML, rec.Header().Get(HeaderContentType))
-		assert.Equal(t, xml.Header+infoStr, rec.Body.String())
+		assert.Equal(t, infoStr, rec.Body.String())
 	}
 }
 
@@ -126,7 +154,7 @@ func TestResponseYAML(t *testing.T) {
 
 	info := struct{ Name, Author string }{"Air", "Aofei Sheng"}
 	infoStr := "name: Air\nauthor: Aofei Sheng\n"
-	if err := c.Response.YAML(info); assert.NoError(t, err) {
+	if err := c.YAML(info); assert.NoError(t, err) {
 		assert.Equal(t, http.StatusOK, rec.Code)
 		assert.Equal(t, MIMEApplicationYAML, rec.Header().Get(HeaderContentType))
 		assert.Equal(t, infoStr, rec.Body.String())
@@ -143,11 +171,16 @@ func TestResponseBlob(t *testing.T) {
 
 	ct := "contentType"
 	b := []byte("blob")
-	if err := c.Response.Blob(ct, b); assert.NoError(t, err) {
+	if err := c.Blob(ct, b); assert.NoError(t, err) {
 		assert.Equal(t, http.StatusOK, rec.Code)
 		assert.Equal(t, ct, rec.Header().Get(HeaderContentType))
 		assert.Equal(t, b, rec.Body.Bytes())
 	}
+
+	c.Response.WriteHeader(http.StatusInternalServerError)
+
+	assert.Equal(t, http.StatusOK, c.Response.StatusCode())
+	assert.Equal(t, len(b), c.Response.Size())
 }
 
 func TestResponseStream(t *testing.T) {
@@ -159,7 +192,7 @@ func TestResponseStream(t *testing.T) {
 	c.feed(req, rec)
 
 	s := "response from a stream"
-	if err := c.Response.Stream(MIMEOctetStream, strings.NewReader(s)); assert.NoError(t, err) {
+	if err := c.Stream(MIMEOctetStream, strings.NewReader(s)); assert.NoError(t, err) {
 		assert.Equal(t, http.StatusOK, rec.Code)
 		assert.Equal(t, MIMEOctetStream, rec.Header().Get(HeaderContentType))
 		assert.Equal(t, s, rec.Body.String())
@@ -176,7 +209,7 @@ func TestResponseFile(t *testing.T) {
 
 	f := "air.go"
 	b, _ := ioutil.ReadFile(f)
-	if err := c.Response.File(f); assert.NoError(t, err) {
+	if err := c.File(f); assert.NoError(t, err) {
 		assert.Equal(t, http.StatusOK, rec.Code)
 		assert.Equal(t, MIMETextPlain, rec.Header().Get(HeaderContentType))
 		assert.Equal(t, b, rec.Body.Bytes())
@@ -194,7 +227,7 @@ func TestResponseAttachment(t *testing.T) {
 	f := "air.go"
 	h := "attachment; filename=" + f
 	b, _ := ioutil.ReadFile(f)
-	if err := c.Response.Attachment(f, f); assert.NoError(t, err) {
+	if err := c.Attachment(f, f); assert.NoError(t, err) {
 		assert.Equal(t, http.StatusOK, rec.Code)
 		assert.Equal(t, h, rec.Header().Get(HeaderContentDisposition))
 		assert.Equal(t, b, rec.Body.Bytes())
@@ -212,7 +245,7 @@ func TestResponseInline(t *testing.T) {
 	f := "air.go"
 	h := "inline; filename=" + f
 	b, _ := ioutil.ReadFile(f)
-	if err := c.Response.Inline(f, f); assert.NoError(t, err) {
+	if err := c.Inline(f, f); assert.NoError(t, err) {
 		assert.Equal(t, http.StatusOK, rec.Code)
 		assert.Equal(t, h, rec.Header().Get(HeaderContentDisposition))
 		assert.Equal(t, b, rec.Body.Bytes())
@@ -227,7 +260,7 @@ func TestResponseNoContent(t *testing.T) {
 
 	c.feed(req, rec)
 
-	c.Response.NoContent()
+	c.NoContent()
 	assert.Equal(t, http.StatusOK, rec.Code)
 	assert.Equal(t, "", rec.Header().Get(HeaderContentDisposition))
 	assert.Equal(t, "", rec.Body.String())
@@ -242,7 +275,7 @@ func TestResponseRedirect(t *testing.T) {
 	c.feed(req, rec)
 
 	url := "https://github.com/sheng/air"
-	if err := c.Response.Redirect(http.StatusMovedPermanently, url); assert.NoError(t, err) {
+	if err := c.Redirect(http.StatusMovedPermanently, url); assert.NoError(t, err) {
 		assert.Equal(t, http.StatusMovedPermanently, rec.Code)
 		assert.Equal(t, url, rec.Header().Get(HeaderLocation))
 		assert.Equal(t, "", rec.Body.String())
