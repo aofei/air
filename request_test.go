fa848098c03f2fc90874e46b422c73352d75af64
sheng <sheng@aofei.org>
2017-02-21 18:33:55 +0800

    test: update request_test.go

--- request_test.go
+++ request_test.go
@@ -9,19 +9,24 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestRequestFormValue(t *testing.T) {
+func TestRequestQueryValueAndFormValue(t *testing.T) {
 	a := New()
 	c := NewContext(a)
 
 	vs := make(url.Values)
 	vs.Set("name", "Air")
 	vs.Set("author", "Aofei Sheng")
-	req, _ := http.NewRequest(POST, "/", strings.NewReader(vs.Encode()))
+
+	req, _ := http.NewRequest(POST, "/?"+vs.Encode(), strings.NewReader(vs.Encode()))
 	req.Header.Add(HeaderContentType, MIMEApplicationForm)
 
 	c.feed(req, nil)
 
-	assert.Equal(t, "Air", c.Request.FormValue("name"))
-	assert.Equal(t, "Aofei Sheng", c.Request.FormValue("author"))
-	assert.Equal(t, vs, c.Request.FormValues())
+	assert.Equal(t, "Air", c.QueryValue("name"))
+	assert.Equal(t, "Aofei Sheng", c.QueryValue("author"))
+	assert.Equal(t, vs.Get("name"), c.QueryValues().Get("name"))
+
+	assert.Equal(t, "Air", c.FormValue("name"))
+	assert.Equal(t, "Aofei Sheng", c.FormValue("author"))
+	assert.Equal(t, vs.Get("name"), c.FormValues().Get("name"))
 }
