92cdeef8cd7aa79ee0b5e316855a1f2c0be4c950
Aofei Sheng <aofei@sheng.ws>
2017-10-20 14:19:44 +0800

    refactor: unify url.go and url_test.go

--- url_test.go
+++ url_test.go
@@ -8,11 +8,10 @@ import (
 )
 
 func TestURL(t *testing.T) {
-	su, err := url.Parse("https://example.com/foo/bar?foo=bar#foobar")
-	assert.NotNil(t, su)
-	assert.Nil(t, err)
+	su, _ := url.Parse("https://example.com/foo/bar?foo=bar#foobar")
 
 	u := newURL(su)
+	assert.NotNil(t, u)
 	assert.Equal(t, su.Scheme, u.Scheme)
 	assert.Equal(t, su.Host, u.Host)
 	assert.Equal(t, su.EscapedPath(), u.Path)
