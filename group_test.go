250e617c4a2c40d5cad0b751914c16f10ccc000f
Aofei Sheng <aofei@sheng.ws>
2017-11-04 13:14:33 +0800

    test: add group_test.go

---
+++ group_test.go
@@ -0,0 +1,21 @@
+package air
+
+import "testing"
+
+func TestGroup(t *testing.T) {
+	g := &Group{
+		Prefix: "/group",
+	}
+
+	g.GET("/", nil)
+	g.HEAD("/", nil)
+	g.POST("/", nil)
+	g.PUT("/", nil)
+	g.PATCH("/", nil)
+	g.DELETE("/", nil)
+	g.CONNECT("/", nil)
+	g.OPTIONS("/", nil)
+	g.TRACE("/", nil)
+	g.STATIC("/", "")
+	g.FILE("/", "")
+}
