37a59abf6fa5e3407e80a2f3c499cb01eb96522b
Aofei Sheng <aofei@aofeisheng.com>
2018-11-30 15:14:13 +0800

    test: add minifier_test.go

---
+++ minifier_test.go
@@ -0,0 +1,29 @@
+package air
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewMinifier(t *testing.T) {
+	a := New()
+	m := a.minifier
+
+	assert.NotNil(t, m)
+	assert.NotNil(t, m.a)
+	assert.NotNil(t, m.minifier)
+}
+
+func TestMinifierMinify(t *testing.T) {
+	a := New()
+	m := a.minifier
+
+	b, err := m.minify("", nil)
+	assert.NoError(t, err)
+	assert.Empty(t, string(b))
+
+	b, err = m.minify("text/html", []byte("<a href=\"/\">Go Home</a>"))
+	assert.NoError(t, err)
+	assert.Equal(t, "<a href=/>Go Home</a>", string(b))
+}
