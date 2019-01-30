60b6afe927e2c909b898b311d9022c566055a1df
Aofei Sheng <aofei@aofeisheng.com>
2019-01-31 01:01:33 +0800

    refactor: simplify configuration

--- air.go
+++ air.go
@@ -17,6 +17,7 @@ import (
 	"unsafe"
 
 	"github.com/BurntSushi/toml"
+	"github.com/mitchellh/mapstructure"
 	ini "gopkg.in/ini.v1"
 	yaml "gopkg.in/yaml.v2"
 )
@@ -26,13 +27,13 @@ type Air struct {
 	// AppName is the name of the current web application.
 	//
 	// The default value is "air".
-	AppName string `json:"app_name" xml:"app_name" toml:"app_name" yaml:"app_name" ini:"app_name"`
+	AppName string `mapstructure:"app_name"`
 
 	// MaintainerEmail is the e-mail address of the one who is responsible
 	// for maintaining the current web application.
 	//
 	// The default value is "".
-	MaintainerEmail string `json:"maintainer_email" xml:"maintainer_email" toml:"maintainer_email" yaml:"maintainer_email" ini:"maintainer_email"`
+	MaintainerEmail string `mapstructure:"maintainer_email"`
 
 	// DebugMode indicates whether the current web application is in debug
 	// mode.
@@ -40,61 +41,61 @@ type Air struct {
 	// ATTENTION: Some features will be affected in debug mode.
 	//
 	// The default value is false.
-	DebugMode bool `json:"debug_mode" xml:"debug_mode" toml:"debug_mode" yaml:"debug_mode" ini:"debug_mode"`
+	DebugMode bool `mapstructure:"debug_mode"`
 
 	// Address is the TCP address that the server listens on.
 	//
 	// The default value is ":8080".
-	Address string `json:"address" xml:"address" toml:"address" yaml:"address" ini:"address"`
+	Address string `mapstructure:"address"`
 
 	// HostWhitelist is the hosts allowed by the server.
 	//
 	// It only works when the `DebugMode` is false.
 	//
 	// The default value is nil.
-	HostWhitelist []string `json:"host_whitelist" xml:"host_whitelist" toml:"host_whitelist" yaml:"host_whitelist" ini:"host_whitelist"`
+	HostWhitelist []string `mapstructure:"host_whitelist"`
 
 	// ReadTimeout is the maximum duration the server reads the request.
 	//
 	// The default value is 0.
-	ReadTimeout time.Duration `json:"read_timeout" xml:"read_timeout" toml:"read_timeout" yaml:"read_timeout" ini:"read_timeout"`
+	ReadTimeout time.Duration `mapstructure:"read_timeout"`
 
 	// ReadHeaderTimeout is the amount of time allowed the server reads the
 	// request headers.
 	//
 	// The default value is 0.
-	ReadHeaderTimeout time.Duration `json:"read_header_timeout" xml:"read_header_timeout" toml:"read_header_timeout" yaml:"read_header_timeout" ini:"read_header_timeout"`
+	ReadHeaderTimeout time.Duration `mapstructure:"read_header_timeout"`
 
 	// WriteTimeout is the maximum duration the server writes an response.
 	//
 	// The default value is 0.
-	WriteTimeout time.Duration `json:"write_timeout" xml:"write_timeout" toml:"write_timeout" yaml:"write_timeout" ini:"write_timeout"`
+	WriteTimeout time.Duration `mapstructure:"write_timeout"`
 
 	// IdleTimeout is the maximum amount of time the server waits for the
 	// next request. If it is zero, the value of `ReadTimeout` is used. If
 	// both are zero, the value `ReadHeaderTimeout` is used.
 	//
 	// The default value is 0.
-	IdleTimeout time.Duration `json:"idle_timeout" xml:"idle_timeout" toml:"idle_timeout" yaml:"idle_timeout" ini:"idle_timeout"`
+	IdleTimeout time.Duration `mapstructure:"idle_timeout"`
 
 	// MaxHeaderBytes is the maximum number of bytes the server will read
 	// parsing the request header's names and values, including the request
 	// line.
 	//
 	// The default value is 1048576.
-	MaxHeaderBytes int `json:"max_header_bytes" xml:"max_header_bytes" toml:"max_header_bytes" yaml:"max_header_bytes" ini:"max_header_bytes"`
+	MaxHeaderBytes int `mapstructure:"max_header_bytes"`
 
 	// TLSCertFile is the path to the TLS certificate file used when
 	// starting the server.
 	//
 	// The default value is "".
-	TLSCertFile string `json:"tls_cert_file" xml:"tls_cert_file" toml:"tls_cert_file" yaml:"tls_cert_file" ini:"tls_cert_file"`
+	TLSCertFile string `mapstructure:"tls_cert_file"`
 
 	// TLSKeyFile is the path to the TLS key file used when starting the
 	// server.
 	//
 	// The default value is "".
-	TLSKeyFile string `json:"tls_key_file" xml:"tls_key_file" toml:"tls_key_file" yaml:"tls_key_file" ini:"tls_key_file"`
+	TLSKeyFile string `mapstructure:"tls_key_file"`
 
 	// ACMEEnabled indicates whether the ACME is enabled.
 	//
@@ -102,45 +103,45 @@ type Air struct {
 	// `TLSCertFile` and the `TLSKeyFile` are empty.
 	//
 	// The default value is false.
-	ACMEEnabled bool `json:"acme_enabled" xml:"acme_enabled" toml:"acme_enabled" yaml:"acme_enabled" ini:"acme_enabled"`
+	ACMEEnabled bool `mapstructure:"acme_enabled"`
 
 	// ACMECertRoot is the root of the ACME certificates.
 	//
 	// The default value is "acme-certs".
-	ACMECertRoot string `json:"acme_cert_root" xml:"acme_cert_root" toml:"acme_cert_root" yaml:"acme_cert_root" ini:"acme_cert_root"`
+	ACMECertRoot string `mapstructure:"acme_cert_root"`
 
 	// HTTPSEnforced indicates whether the HTTPS is enforced.
 	//
 	// The default value is false.
-	HTTPSEnforced bool `json:"https_enforced" xml:"https_enforced" toml:"https_enforced" yaml:"https_enforced" ini:"https_enforced"`
+	HTTPSEnforced bool `mapstructure:"https_enforced"`
 
 	// WebSocketHandshakeTimeout is the maximum amount of time the server
 	// waits for the WebSocket handshake to complete.
 	//
 	// The default value is 0.
-	WebSocketHandshakeTimeout time.Duration `json:"websocket_handshake_timeout" xml:"websocket_handshake_timeout" toml:"websocket_handshake_timeout" yaml:"websocket_handshake_timeout" ini:"websocket_handshake_timeout"`
+	WebSocketHandshakeTimeout time.Duration `mapstructure:"websocket_handshake_timeout"`
 
 	// WebSocketSubprotocols is the supported WebSocket subprotocols of the
 	// server.
 	//
 	// The default value is nil.
-	WebSocketSubprotocols []string `json:"websocket_subprotocols" xml:"websocket_subprotocols" toml:"websocket_subprotocols" yaml:"websocket_subprotocols" ini:"websocket_subprotocols"`
+	WebSocketSubprotocols []string `mapstructure:"websocket_subprotocols"`
 
 	// NotFoundHandler is a `Handler` that returns not found error.
 	//
 	// The default value is the `DefaultNotFoundHandler`.
-	NotFoundHandler func(*Request, *Response) error `json:"-" xml:"-" toml:"-" yaml:"-" ini:"-"`
+	NotFoundHandler func(*Request, *Response) error `mapstructure:"-"`
 
 	// MethodNotAllowedHandler is a `Handler` that returns method not
 	// allowed error.
 	//
 	// The default value is the `DefaultMethodNotAllowedHandler`.
-	MethodNotAllowedHandler func(*Request, *Response) error `json:"-" xml:"-" toml:"-" yaml:"-" ini:"-"`
+	MethodNotAllowedHandler func(*Request, *Response) error `mapstructure:"-"`
 
 	// ErrorHandler is the centralized error handler of the server.
 	//
 	// The default value is the `DefaultErrorHandler`.
-	ErrorHandler func(error, *Request, *Response) `json:"-" xml:"-" toml:"-" yaml:"-" ini:"-"`
+	ErrorHandler func(error, *Request, *Response) `mapstructure:"-"`
 
 	// ErrorLogger is the `log.Logger` that logs errors that occur in the
 	// server.
@@ -148,27 +149,27 @@ type Air struct {
 	// If nil, logging is done via the log package's standard logger.
 	//
 	// The default value is nil.
-	ErrorLogger *log.Logger `json:"-" xml:"-" toml:"-" yaml:"-" ini:"-"`
+	ErrorLogger *log.Logger `mapstructure:"-"`
 
 	// Pregases is the `Gas` chain stack that performs before routing.
 	//
 	// The default value is nil.
-	Pregases []Gas `json:"-" xml:"-" toml:"-" yaml:"-" ini:"-"`
+	Pregases []Gas `mapstructure:"-"`
 
 	// Gases is the `Gas` chain stack that performs after routing.
 	//
 	// The default value is nil.
-	Gases []Gas `json:"-" xml:"-" toml:"-" yaml:"-" ini:"-"`
+	Gases []Gas `mapstructure:"-"`
 
 	// AutoPushEnabled indicates whether the auto push is enabled.
 	//
 	// The default value is false.
-	AutoPushEnabled bool `json:"auto_push_enabled" xml:"auto_push_enabled" toml:"auto_push_enabled" yaml:"auto_push_enabled" ini:"auto_push_enabled"`
+	AutoPushEnabled bool `mapstructure:"auto_push_enabled"`
 
 	// MinifierEnabled indicates whether the minifier is enabled.
 	//
 	// The default value is false.
-	MinifierEnabled bool `json:"minifier_enabled" xml:"minifier_enabled" toml:"minifier_enabled" yaml:"minifier_enabled" ini:"minifier_enabled"`
+	MinifierEnabled bool `mapstructure:"minifier_enabled"`
 
 	// MinifierMIMETypes is the MIME types that will be minified.
 	// Unsupported MIME types will be silently ignored.
@@ -176,72 +177,72 @@ type Air struct {
 	// The default value is ["text/html", "text/css",
 	// "application/javascript", "application/json", "application/xml",
 	// "image/svg+xml"].
-	MinifierMIMETypes []string `json:"minifier_mime_types" xml:"minifier_mime_types" toml:"minifier_mime_types" yaml:"minifier_mime_types" ini:"minifier_mime_types"`
+	MinifierMIMETypes []string `mapstructure:"minifier_mime_types"`
 
 	// GzipEnabled indicates whether the gzip is enabled.
 	//
 	// The default value is false.
-	GzipEnabled bool `json:"gzip_enabled" xml:"gzip_enabled" toml:"gzip_enabled" yaml:"gzip_enabled" ini:"gzip_enabled"`
+	GzipEnabled bool `mapstructure:"gzip_enabled"`
 
 	// GzipCompressionLevel is the compression level of the gzip.
 	//
 	// The default value is `gzip.DefaultCompression`.
-	GzipCompressionLevel int `json:"gzip_compression_level" xml:"gzip_compression_level" toml:"gzip_compression_level" yaml:"gzip_compression_level" ini:"gzip_compression_level"`
+	GzipCompressionLevel int `mapstructure:"gzip_compression_level"`
 
 	// GzipMIMETypes is the MIME types that will be gzipped.
 	//
 	// The default value is ["text/plain", "text/html", "text/css",
 	// "application/javascript", "application/json", "application/xml",
 	// "image/svg+xml"].
-	GzipMIMETypes []string `json:"gzip_mime_types" xml:"gzip_mime_types" toml:"gzip_mime_types" yaml:"gzip_mime_types" ini:"gzip_mime_types"`
+	GzipMIMETypes []string `mapstructure:"gzip_mime_types"`
 
 	// TemplateRoot is the root of the HTML templates. All the HTML
 	// templates inside it will be recursively parsed into the renderer.
 	//
 	// The default value is "templates".
-	TemplateRoot string `json:"template_root" xml:"template_root" toml:"template_root" yaml:"template_root" ini:"template_root"`
+	TemplateRoot string `mapstructure:"template_root"`
 
 	// TemplateExts is the filename extensions of the HTML templates used to
 	// distinguish the HTML template files in the `TemplateRoot` when
 	// parsing them into the renderer.
 	//
 	// The default value is [".html"].
-	TemplateExts []string `json:"template_exts" xml:"template_exts" toml:"template_exts" yaml:"template_exts" ini:"template_exts"`
+	TemplateExts []string `mapstructure:"template_exts"`
 
 	// TemplateLeftDelim is the left side of the HTML template delimiter the
 	// renderer renders the HTML templates.
 	//
 	// The default value is "{{".
-	TemplateLeftDelim string `json:"template_left_delim" xml:"template_left_delim" toml:"template_left_delim" yaml:"template_left_delim" ini:"template_left_delim"`
+	TemplateLeftDelim string `mapstructure:"template_left_delim"`
 
 	// TemplateRightDelim is the right side of the HTML template delimiter
 	// the renderer renders the HTML templates.
 	//
 	// The default value is "}}".
-	TemplateRightDelim string `json:"template_right_delim" xml:"template_right_delim" toml:"template_right_delim" yaml:"template_right_delim" ini:"template_right_delim"`
+	TemplateRightDelim string `mapstructure:"template_right_delim"`
 
 	// TemplateFuncMap is the HTML template function map the renderer
 	// renders the HTML templates.
 	//
 	// The default value contains strlen, substr and timefmt.
-	TemplateFuncMap template.FuncMap `json:"-" xml:"-" toml:"-" yaml:"-" ini:"-"`
+	TemplateFuncMap template.FuncMap `mapstructure:"-"`
 
 	// CofferEnabled indicates whether the coffer is enabled.
 	//
 	// The default value is false.
-	CofferEnabled bool `json:"coffer_enabled" xml:"coffer_enabled" toml:"coffer_enabled" yaml:"coffer_enabled" ini:"coffer_enabled"`
+	CofferEnabled bool `mapstructure:"coffer_enabled"`
 
 	// CofferMaxMemoryBytes is the maximum number of bytes of the runtime
 	// memory the coffer will use.
 	//
 	// The default value is 33554432.
-	CofferMaxMemoryBytes int `json:"coffer_max_memory_bytes" xml:"coffer_max_memory_bytes" toml:"coffer_max_memory_bytes" yaml:"coffer_max_memory_bytes" ini:"coffer_max_memory_bytes"`
+	CofferMaxMemoryBytes int `mapstructure:"coffer_max_memory_bytes"`
 
 	// AssetRoot is the root of the asset files. All the asset files inside
 	// it will be recursively parsed into the coffer.
 	//
 	// The default value is "assets".
-	AssetRoot string `json:"asset_root" xml:"asset_root" toml:"asset_root" yaml:"asset_root" ini:"asset_root"`
+	AssetRoot string `mapstructure:"asset_root"`
 
 	// AssetExts is the filename extensions of the asset files used to
 	// distinguish the asset files in the `AssetRoot` when loading them into
@@ -249,24 +250,24 @@ type Air struct {
 	//
 	// The default value is [".html", ".css", ".js", ".json", ".xml",
 	// ".svg", ".jpg", ".jpeg", ".png", ".gif"].
-	AssetExts []string `json:"asset_exts" xml:"asset_exts" toml:"asset_exts" yaml:"asset_exts" ini:"asset_exts"`
+	AssetExts []string `mapstructure:"asset_exts"`
 
 	// I18nEnabled indicates whether the i18n is enabled.
 	//
 	// The default value is false.
-	I18nEnabled bool `json:"i18n_enabled" xml:"i18n_enabled" toml:"i18n_enabled" yaml:"i18n_enabled" ini:"i18n_enabled"`
+	I18nEnabled bool `mapstructure:"i18n_enabled"`
 
 	// LocaleRoot is the root of the locale files. All the locale files
 	// inside it will be parsed into the i18n.
 	//
 	// The default value is "locales".
-	LocaleRoot string `json:"locale_root" xml:"locale_root" toml:"locale_root" yaml:"locale_root" ini:"locale_root"`
+	LocaleRoot string `mapstructure:"locale_root"`
 
 	// LocaleBase is the base of the locale files. It will be used when a
 	// locale file cannot be found.
 	//
 	// The default value is "en-US".
-	LocaleBase string `json:"locale_base" xml:"locale_base" toml:"locale_base" yaml:"locale_base" ini:"locale_base"`
+	LocaleBase string `mapstructure:"locale_base"`
 
 	// ConfigFile is the path to the configuration file that will be parsed
 	// into the matching fields before starting the server.
@@ -279,10 +280,8 @@ type Air struct {
 	//
 	// ".yaml|.yml" -> The configuration file is YAML-based.
 	//
-	// ".ini|.cfg|.conf|.txt" -> The configuration file is INI-based.
-	//
 	// The default value is "".
-	ConfigFile string `json:"-" xml:"-" toml:"-" yaml:"-" ini:"-"`
+	ConfigFile string `mapstructure:"-"`
 
 	errorLogger                  *log.Logger
 	server                       *server
@@ -541,25 +540,28 @@ func (a *Air) Serve() error {
 			return err
 		}
 
+		m := map[string]interface{}{}
 		switch strings.ToLower(filepath.Ext(a.ConfigFile)) {
 		case ".json":
-			err = json.Unmarshal(b, a)
+			err = json.Unmarshal(b, &m)
 		case ".xml":
-			err = xml.Unmarshal(b, a)
+			err = xml.Unmarshal(b, &m)
 		case ".toml":
-			err = toml.Unmarshal(b, a)
+			err = toml.Unmarshal(b, &m)
 		case ".yaml", ".yml":
-			err = yaml.Unmarshal(b, a)
+			err = yaml.Unmarshal(b, &m)
 		case ".ini", ".cfg", ".conf", ".txt":
 			var cfg *ini.File
 			if cfg, err = ini.Load(b); err != nil {
 				return err
 			}
 
-			err = cfg.MapTo(a)
+			err = cfg.MapTo(&m)
 		}
 
 		if err != nil {
+			return err
+		} else if err := mapstructure.Decode(m, a); err != nil {
 			return err
 		}
 	}
--- go.mod
+++ go.mod
@@ -11,15 +11,16 @@ require (
 	github.com/gorilla/websocket v1.4.0
 	github.com/jtolds/gls v4.2.1+incompatible // indirect
 	github.com/kr/pretty v0.1.0 // indirect
+	github.com/mitchellh/mapstructure v1.1.2
 	github.com/smartystreets/assertions v0.0.0-20190116191733-b6c0e53d7304 // indirect
 	github.com/smartystreets/goconvey v0.0.0-20181108003508-044398e4856c // indirect
 	github.com/stretchr/testify v1.3.0
 	github.com/tdewolff/minify/v2 v2.3.8
 	github.com/vmihailenco/msgpack v4.0.2+incompatible
-	golang.org/x/crypto v0.0.0-20190129210102-ccddf3741a0c
+	golang.org/x/crypto v0.0.0-20190130090550-b01c7a725664
 	golang.org/x/net v0.0.0-20190125091013-d26f9f9a57f3
 	golang.org/x/sync v0.0.0-20181221193216-37e7f081c4d4 // indirect
-	golang.org/x/sys v0.0.0-20190129075346-302c3dd5f1cc // indirect
+	golang.org/x/sys v0.0.0-20190130150945-aca44879d564 // indirect
 	golang.org/x/text v0.3.0
 	google.golang.org/appengine v1.4.0 // indirect
 	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
